80ed526d4e305f102e89e8e0c1af6e3c2d6609c0
Aarya Jain <aaryaaa.jain22@gmail.com>
2026-02-23 23:25:56 +0530

    Add HackerRank SQL solution

---
+++ Basic_Select/Weather_Observation_Station_1.sql
@@ -0,0 +1,10 @@
+-- =============================================
+-- Problem: List CITY and STATE from STATION
+-- Source: HackerRank
+-- Description:
+-- Query a list of CITY and STATE
+-- from the STATION table.
+-- =============================================
+
+SELECT CITY, STATE
+FROM STATION;
