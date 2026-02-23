10cd6bb6f52d28a8794dd00b578e261248c284cd
Aarya Jain <aaryaaa.jain22@gmail.com>
2026-02-23 23:18:32 +0530

    Add HackerRank SQL solution

---
+++ Basic_Select/Japanese_Cities_Attributes.sql
@@ -0,0 +1,11 @@
+-- =============================================
+-- Problem: Japanese Cities
+-- Source: HackerRank
+-- Description:
+-- Query all attributes of every Japanese city
+-- in the CITY table. (CountryCode = 'JPN')
+-- =============================================
+
+SELECT *
+FROM CITY
+WHERE CountryCode = 'JPN';
