8210e20371f903eb28c2e37f3da79c6f308d6b43
Aarya Jain <aaryaaa.jain22@gmail.com>
2026-02-24 00:15:34 +0530

    Add HackerRank SQL solution

---
+++ Basic_Select/Employee_Names.sql
@@ -0,0 +1,11 @@
+-- =============================================
+-- Problem: Employee Names in Alphabetical Order
+-- Source: HackerRank
+-- Description:
+-- Query the list of employee names
+-- from the Employee table in alphabetical order.
+-- =============================================
+
+SELECT name
+FROM Employee
+ORDER BY name ASC;
