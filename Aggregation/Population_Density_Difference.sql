4956c5f5ae06a9e0d8b565b4475f72deaf7321fd
Aarya Jain <aaryaaa.jain22@gmail.com>
2026-02-24 00:30:14 +0530

    Add HackerRank SQL Solution

---
+++ Aggregation/Population_Density_Difference.sql
@@ -0,0 +1,10 @@
+-- =============================================
+-- Problem: Difference Between Max and Min Population
+-- Source: HackerRank
+-- Description:
+-- Query the difference between the maximum
+-- and minimum populations in CITY.
+-- =============================================
+
+SELECT MAX(Population) - MIN(Population)
+FROM CITY;
