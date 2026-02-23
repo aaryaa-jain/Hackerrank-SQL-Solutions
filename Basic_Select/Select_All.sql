c83c306b8135e1867a427a2c4fcb90cf60a846e0
Aarya Jain <aaryaaa.jain22@gmail.com>
2026-02-23 23:11:22 +0530

    Add HackerRank SQL solution

---
+++ Basic_Select/Select_All.sql
@@ -0,0 +1,9 @@
+-- =============================================
+-- Problem: Select All Columns from CITY
+-- Source: HackerRank
+-- Description:
+-- Query all columns (attributes) for every row
+-- in the CITY table.
+-- =============================================
+
+SELECT * FROM CITY;
