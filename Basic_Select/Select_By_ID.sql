9cf9e208dedbfaf03f41e607637eabc44afbf550
Aarya Jain <aaryaaa.jain22@gmail.com>
2026-02-23 23:16:48 +0530

    Add HackerRank SQL solution

---
+++ Basic_Select/Select_By_ID.sql
@@ -0,0 +1,11 @@
+-- =============================================
+-- Problem: Select City with Specific ID
+-- Source: HackerRank
+-- Description:
+-- Query all columns for a city in CITY
+-- with the ID 1661.
+-- =============================================
+
+SELECT *
+FROM CITY
+WHERE ID = 1661;
